98b65a826c050e6e957b9558d77638aef94188bc
noob-anmol <anmolghai50@gmail.com>
2024-08-19 01:22:12 +0530

    completed individual country page

--- react-app/src/components/Country/Country.jsx
+++ react-app/src/components/Country/Country.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import "./Country.css";
 import { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import { Navigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const Country = () => {
@@ -19,8 +18,17 @@ const Country = () => {
 		});
 	}, []);
 
+	// const history = useNavigate();
+	// const onBackClick = () => {
+	// 	history.back();
+	// };
+
 	if (!loading) {
-		return <div>loading</div>;
+		return (
+			<div className='country-loading'>
+				<h1 className='country-loading-heading'>Loading...</h1>
+			</div>
+		);
 	}
 	// Loads the below code when the value of Country is fetched.
 	else {
@@ -31,43 +39,74 @@ const Country = () => {
 						Back
 					</button>
 				</section>
-				<div className='country-flag'>
-					<img src={country.flags.png} alt='' />
-				</div>
-				<div className='country-description'>
-					<h1 className='country-heading'>{country.name}</h1>
-					<p className='country-details-para'>
-						<span className='country-details-span'>Native Name: </span>
-						{country.nativeName}
-					</p>
-					<p className='country-details-para'>
-						<span className='country-details-span'>Population: </span>
-						{country.population}
-					</p>
-					<p className='country-details-para'>
-						<span className='country-details-span'>Region: </span>
-						{country.region}
-					</p>
-					<p className='country-details-para'>
-						<span className='country-details-span'>Sub Region: </span>
-						{country.subregion}
-					</p>
-					<p className='country-details-para'>
-						<span className='country-details-span'>Capital: </span>
-						{country.capital}
-					</p>
-					<p className='country-details-para'>
-						<span className='country-details-span'>Top Level Domain: </span>
-						{country.topLevelDomain[0]}
-					</p>
-					<p className='country-details-para'>
-						<span className='country-details-span'>Currencies: </span>
-						{country.currencies[0].code}
-					</p>
-					<p className='country-details-para'>
-						<span className='country-details-span'> Languages: </span>
-						{country.languages[0].name}
-					</p>
+				<div className='country-container'>
+					<div className='country-container-one'>
+						<img src={country.flags.png} alt={country.name} className="country-flg"/>
+					</div>
+					<div className='country-container-two'>
+						<div>
+							<h1 className='country-heading'>{country.name}</h1>
+						</div>
+						<div className='country-column'>
+							<div className="country-column-one">
+								<p className='country-details-para'>
+									<span className='country-details-span'>Native Name: </span>
+									{country.nativeName}
+								</p>
+								<p className='country-details-para'>
+									<span className='country-details-span'>Population: </span>
+									{country.population}
+								</p>
+								<p className='country-details-para'>
+									<span className='country-details-span'>Region: </span>
+									{country.region}
+								</p>
+								<p className='country-details-para'>
+									<span className='country-details-span'>Sub Region: </span>
+									{country.subregion}
+								</p>
+								<p className='country-details-para'>
+									<span className='country-details-span'>Capital: </span>
+									{country.capital}
+								</p>
+							</div>
+							<div className="country-column-two">
+								<p className='country-details-para'>
+									<span className='country-details-span'>
+										Top Level Domain:{" "}
+									</span>
+									{country.topLevelDomain[0]}
+								</p>
+								<p className='country-details-para'>
+									<span className='country-details-span'>Currencies: </span>
+									{country.currencies[0].code}
+								</p>
+								<p className='country-details-para'>
+									<span className='country-details-span'> Languages: </span>
+									{/* {country.languages && country.languages.length > 0 ? (
+										country.languages.map((language) => (
+											<span key={language}>{language}</span>
+										))
+									) : (
+										<span>non</span>
+									)} */}
+
+									{country.languages[0].name}
+								</p>
+							</div>
+							<div></div>
+						</div>
+						<p className='country-details-para borders'>
+							<span className='country-details-span'>Border Countries: </span>
+							{country.borders && country.borders.length > 0 ? (
+								country.borders.map((border) => (
+									<button className='border-countries' key = {border}> {border}</button>
+								))
+							) : (
+								<span>None</span>
+							)}
+						</p>
+					</div>
 				</div>
 			</div>
 		);
--- react-app/src/components/Home/Home.jsx
+++ react-app/src/components/Home/Home.jsx
@@ -4,47 +4,57 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 const Home = () => {
 	const [countries, setCountries] = useState([]);
+
+	//Usestate to check if elements are loaded
+
+	const [load, setLoad] = useState(false);
 	useEffect(() => {
 		axios.get("http://localhost:8000/").then((res) => {
 			setCountries(res.data);
-			res.data.map((country) => {
-				console.log(country.name);
-			});
+			setLoad(true)
 		});
 	}, []);
 
-	return (
-		<div className='home-container'>
-			{countries.map((country) => (
-				<a
-					className='countries'
-					key={country.name}
-					href={`/country?q=${country.name}`}
-				>
-					<img
-						className='country-img'
-						src={country.flags.png}
-						alt={country.name}
-					></img>
-					<div className='country-details'>
-						<h2 className='country-name'>{country.name}</h2>
-						<p>
-							<span className='country-details-span'>Population: </span>
-							{country.population}
-						</p>
-						<p>
-							<span className='country-details-span'>Region: </span>
-							{country.region}
-						</p>
-						<p>
-							<span className='country-details-span'>Capital: </span>
-							{country.capital}
-						</p>
-					</div>
-				</a>
-			))}
-		</div>
-	);
+	if (!load) {
+		return (
+			<div className='country-loading'>
+				<h1 className='country-loading-heading'>Loading...</h1>
+			</div>
+		);
+	} else {
+		return (
+			<div className='home-container'>
+				{countries.map((country) => (
+					<a
+						className='countries'
+						key={country.name}
+						href={`/country?q=${country.name}`}
+					>
+						<img
+							className='country-img'
+							src={country.flags.png}
+							alt={country.name}
+						></img>
+						<div className='country-details'>
+							<h2 className='country-name'>{country.name}</h2>
+							<p>
+								<span className='country-details-span'>Population: </span>
+								{country.population}
+							</p>
+							<p>
+								<span className='country-details-span'>Region: </span>
+								{country.region}
+							</p>
+							<p>
+								<span className='country-details-span'>Capital: </span>
+								{country.capital}
+							</p>
+						</div>
+					</a>
+				))}
+			</div>
+		);
+	}
 };
 
 export default Home;
